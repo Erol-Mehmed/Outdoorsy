343d8950febbce281a7d3768dbb83f0a3a8aeebb
Erol-web <81830899+Erol-web@users.noreply.github.com>
2022-08-26 20:47:22 +0300

    Started and begun creating some of the views

---
+++ src/components/views/Header/Header.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import styles from './Header.module.css';
+
+function Header() {
+
+    return(
+        <header className={styles.header}>
+
+            <h2 className={styles.heading}>Outdoorsy</h2>
+
+        </header>
+    );
+
+};
+
+export default Header;
---
+++ src/components/views/Home/Home.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import Header from '../Header/Header';
+import styles from './Home.module.css';
+
+function Home() {
+    return (
+        <div className={styles.home}>
+            <Header />
+
+        </div>
+    );
+};
+
+export default Home;
