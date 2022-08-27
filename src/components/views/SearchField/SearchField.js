68ccd9503c98d1714a12fc4e68cb6c48df0cad14
Erol-web <81830899+Erol-web@users.noreply.github.com>
2022-08-27 14:32:11 +0300

    Created the the fetch api

--- src/components/views/SearchField/SearchField.js
+++ src/components/views/SearchField/SearchField.js
@@ -24,12 +24,43 @@ import styles from './SearchField.module.css';
 
 function SearchField() {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data.keyword);
-   
+    const onSubmit = async data => {
+        const host = "https://search.outdoorsy.com/rentals?";
+        const url = data.keywords;
+        console.log(data.keywords);
+
+        const options = {
+            method: "GET"
+        }
+
+        try {
+            const response = await fetch(host + url, options);
+
+            if (response.ok !== true) {
+                if (response.status === 403) localStorage.removeItem("user");
+                const error = await response.json();
+                throw new Error(error.message);
+            }
+
+            if (response.status == 204) {
+                return response;
+            } else {
+                const test = await response.json();
+                console.log(test.data[1].attributes.name);
+                return response.json();
+            }
+
+        } catch (error) {
+            alert(error);
+        }
+
+
+    }
+
     return (
 
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            <input name='keyword' {...register('keyword')} className={styles.searchField} placeholder='Enter keyword to find vehicle' required />
+            <input name='keywords' {...register('keywords')} className={styles.searchField} placeholder='Enter keyword to find vehicle' required />
             <button className={styles.submitBtn} type='submit'>Submit</button>
         </form>
 
